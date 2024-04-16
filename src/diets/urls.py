ece4c4a29cb4a47f6cdc458639ac26488429da61
Mateusz Samek <samekmat@gmail.com>
2024-04-16 18:41:49 +0200

    refactor: [skip ci]: rename url paths

--- src/diets/urls.py
+++ src/diets/urls.py
@@ -7,7 +7,7 @@ app_name = "diets"
 
 urlpatterns = [
     path("diets/list/", views.DietListView.as_view(), name="diet-list"),
-    path("diets/<int:pk>/", views.DietView.as_view(), name="diet-detail"),
+    path("diets/<int:pk>/", views.DietDetailView.as_view(), name="diet-detail"),
     path("diets/create/", views.DietCreateView.as_view(), name="diet-create"),
     path("diets/update/<int:pk>/", views.DietUpdateView.as_view(), name="diet-update"),
     path("diets/delete/<int:pk>/", views.DietDeleteView.as_view(), name="diet-delete"),
