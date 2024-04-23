33cb788c936447d970638558c2c866184760b2c6
Mateusz Samek <samekmat@gmail.com>
2024-04-23 23:52:56 +0200

    refactor(settings directory): [skip ci]: rename PlanDiet into core

--- src/core/asgi.py
+++ src/core/asgi.py
@@ -1,4 +1,4 @@
-"""ASGI config for PlanDiet project.
+"""ASGI config for core project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -10,6 +10,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PlanDiet.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = get_asgi_application()
--- src/core/settings.py
+++ src/core/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "PlanDiet.urls"
+ROOT_URLCONF = "core.urls"
 
 TEMPLATES = [
     {
@@ -79,7 +79,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "PlanDiet.wsgi.application"
+WSGI_APPLICATION = "core.wsgi.application"
 
 
 # Database
