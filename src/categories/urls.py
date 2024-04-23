72e0cdce34a784fc33ad97a3fd201a6e0eebf7e9
Mateusz Samek <samekmat@gmail.com>
2024-04-23 23:30:00 +0200

    refactor: [skip ci]: change listview templates and also change id into pk in categories urls

--- src/categories/urls.py
+++ src/categories/urls.py
@@ -7,8 +7,8 @@ app_name = "categories"
 
 urlpatterns = [
     path("categories/", views.CategoryListView.as_view(), name="categories"),
-    path("categories/<int:id>/", views.CategoryDetailView.as_view(), name="category-detail"),
-    path("categories/delete/<int:id>/", views.CategoryDeleteView.as_view(), name="category-delete"),
+    path("categories/<int:pk>/", views.CategoryDetailView.as_view(), name="category-detail"),
+    path("categories/delete/<int:pk>/", views.CategoryDeleteView.as_view(), name="category-delete"),
     path("categories/create/", views.CategoryCreateView.as_view(), name="category-create"),
-    path("categories/update/", views.CategoryUpdateView.as_view(), name="category-update"),
+    path("categories/update/<int:pk>", views.CategoryUpdateView.as_view(), name="category-update"),
 ]
