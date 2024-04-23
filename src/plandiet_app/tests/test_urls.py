41bba712fdc75a9b969853b375159fb55ea31d04
Mateusz Samek <samekmat@gmail.com>
2024-04-23 23:12:58 +0200

    test: [skip ci]: change assert for test_index_url accordingly to previous changes

--- src/plandiet_app/tests/test_urls.py
+++ src/plandiet_app/tests/test_urls.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 class UrlsTest(SimpleTestCase):
     def test_index_url(self):
         url = reverse("index")
-        self.assertEqual(url, "/index/")
+        self.assertEqual(url, "/")
 
     def test_macro_calculator_url(self):
         url = reverse("macro-calc")
